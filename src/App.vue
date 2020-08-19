7a867cfacb9afa3ed6040226d928445ef7a838dc
luzcry <mcrodriguezt618@gmail.com>
2020-08-18 23:34:12 -0500

    componente newquote y metodo que agrega nuevos quotes al padre

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="container">
+    <NewQuote @quoteAdded="NewQuote"></NewQuote>
     <QuotesGrid :quotes="quotes"></QuotesGrid>
   </div>
 </template>
 
 <script>
 import QuotesGrid from "./components/QuotesGrid";
+import NewQuote from "./components/NewQuote";
 export default {
-  components: { QuotesGrid },
+  components: { QuotesGrid, NewQuote },
   data: function () {
     return {
       quotes: ["Just a quote to see something"],
       maxQuotes: 10,
     };
+  },
+  methods: {
+    NewQuote(quote) {
+      this.quotes.push(quote);
+    },
   },
 };
 </script>
